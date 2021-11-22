79b30cd38b270506a285234ca37d5fcc2bdc98ad
Hayden Woolstenhulme <hayd1234@gmail.com>
2021-11-21 18:01:15 -0700

    added copy for roomcode

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -41,6 +41,7 @@ export default class Home extends Component {
     this.getPeople = this.getPeople.bind(this)
     this.newUser = this.newUser.bind(this)
     this.toggleBack = this.toggleBack.bind(this)
+    this.copyText = this.copyText.bind(this)
   }
   componentDidMount() {
     socket.on("connect", () => {
@@ -138,6 +139,9 @@ export default class Home extends Component {
   toggleBack(){
     this.setState({room: '', create:'', showBack: !this.state.showBack})
   }
+  copyText(){
+    navigator.clipboard.writeText(this.state.room);
+  }
   render() {
     const styles = {
       div: {
@@ -198,6 +202,9 @@ export default class Home extends Component {
 
     let content = <div>
       <h3 style={{ position: 'absolute', top: '80px', left: '10px' }}>Room Code: {this.state.room}</h3>
+      <svg id="copy" onClick={this.copyText} style={{position:'absolute', top:'100px', left:'190px', cursor:'pointer'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
+        <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
+      </svg>
       <h3 style={{ position: 'absolute', top: '120px', left: '10px' }}>People in Room:</h3>
       <div style={{ position: 'absolute', top: '160px', left: '10px' }}>{this.state.people.map((el, i) => {
         return (
