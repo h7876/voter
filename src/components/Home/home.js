9abf04eac1fe2792f3c82383ba45485e40bf9d69
Hayden Woolstenhulme <hayd1234@gmail.com>
2022-07-28 19:37:09 -0700

    added voted checkmark (no logic yet)

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -224,8 +224,11 @@ export default class Home extends Component {
       <h3 style={{ position: 'absolute', top: '120px', left: '10px' }}>People in Room:</h3>
       <div style={{ position: 'absolute', top: '160px', left: '10px' }}>{this.state.people.map((el, i) => {
         return (
-          <div key={i + el}>
+          <div key={i + el} style={{display: 'flex', width: 'auto', minWidth:'20px'}}>
             <h3>{el}</h3>
+            <svg style={{position: 'relative', top:'22px', left:'10px'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </svg> 
           </div>
         );
       })}
