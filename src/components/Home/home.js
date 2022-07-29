a784e1c6755fcdf2ebefd733c600e1ff1ce03abd
Hayden Woolstenhulme <hayd1234@gmail.com>
2022-07-29 16:21:55 -0700

    conditionally render voted checkmark

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -226,12 +226,16 @@ export default class Home extends Component {
         return (
           <div key={i + el} style={{display: 'flex', width: 'auto', minWidth:'20px'}}>
             <h3>{el}</h3>
-            <svg style={{position: 'relative', top:'22px', left:'10px'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-            </svg> 
+            {this.state.votes.map((vote, i)=> {
+              if(vote.name == el){
+                return (
+                  <svg key={vote + el} style={{position: 'relative', top:'22px', left:'10px'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                  </svg> 
+                )}
+            })}
           </div>
-        );
-      })}
+        );})}
       </div>
 
       {this.state.isHost ?
